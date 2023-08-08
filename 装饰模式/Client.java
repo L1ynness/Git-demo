1be61362b349340d2a25ceaddd5f9c618d58a6d2
L1ynness <1366606525@qq.com>
2023-08-08 17:12:27 +0800

    修改了装饰模式的Client.java文件

--- 装饰模式/Client.java
+++ 装饰模式/Client.java
@@ -2,10 +2,10 @@ package 装饰模式;
 
 public class Client {
     public static void main(String[] args) {
+	//实现对象注入
         Snack snack = new PlainNoodles();
         snack = new BeefDecorator(snack);
         snack = new EggDecorator(snack);
         System.out.println(snack.getName());
         System.out.println(snack.getPrice() + "元");
     }
-}
