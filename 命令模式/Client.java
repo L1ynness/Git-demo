cff760b5f1f4e4b49fee03aec4adf8a5db911b0b
L1ynness <1366606525@qq.com>
2023-08-08 13:33:16 +0800

    修改了Client.java

--- 命令模式/Client.java
+++ 命令模式/Client.java
@@ -3,9 +3,10 @@ package 命令模式;
 public class Client {
     public static void main(String[] args) {
         Command FishCommand,ChickenCommand,MeatCommand;
-        FishCommand = new FishCommand();
+	FishCommand = new FishCommand();
         ChickenCommand = new ChickenCommand();
         MeatCommand = new MeatCommand();
+	//创建控制器对象
         Controller controller = new Controller(FishCommand,MeatCommand,ChickenCommand);
         controller.execute();
     }
